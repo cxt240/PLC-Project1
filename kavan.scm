6086da94cadaf00b3dbead950f332ddc7e72b88a
cxt240 <cxt240@case.edu>
2017-02-18 17:47:59 -0500

    basic arithmetic
    updated filter to fetch variables and check each substatement

--- kavan.scm
+++ kavan.scm
@@ -1,3 +1,28 @@
+(load "Interpreter2.scm")
+ 
+(define filter
+  (lambda (stmt var val)  ; car stmt is op
+    (cond
+      ( (or (null? stmt) (null? var) (null? val)) '() )
+      (  (eq? '+ (car stmt)) (sum (check (cadr stmt) var val) (check (cadr (cdr stmt)) var val)))    ; + op
+      (  (eq? '- (car stmt)) (diff (check (cadr stmt) var val) (check (cadr (cdr stmt)) var val)))    ; + op
+      (  (eq? '* (car stmt)) (prod (check (cadr stmt) var val) (check (cadr (cdr stmt)) var val)))    ; + op
+      (  (eq? '/ (car stmt)) (div (check (cadr stmt) var val) (check (cadr (cdr stmt)) var val)))    ; + op
+      (  (eq? '% (car stmt)) (% (check (cadr stmt) var val) (check (cadr (cdr stmt)) var val)))    ; + op
+
+    )
+  )
+)
+
+;checks if the substatement is an atom or not
+(define check
+  (lambda (x var val)
+    (cond
+      ((list? x) (filter var val)) ; list, just do another filter call
+      ((number? x) x)              ; number, return the number
+      (else (getValue val (getIndex var x))))))   ; else get the value of the variable
+
+
 (define %
   (lambda (a b)
     (cond
@@ -13,20 +38,6 @@
       ( (or (null? a) (null? b)) '())
       ( (eq? a b) #t )
       (else #f)
-    )
-  )
-)
-
-(define filter
-  (lambda (stmt var val)  ; car stmt is op
-    (cond
-      ( (or (null? stmt) (null? var) (null? val)) '() )
-      (  (eq? '+ (car stmt)) (sum (filter  ) ( )  ))    ; + op
-      (  (eq? '- (car stmt)) (diff (filter  ) ( )  ))    ; + op
-      (  (eq? '* (car stmt)) (prod (filter  ) ( )  ))    ; + op
-      (  (eq? '/ (car stmt)) (div (filter  ) ( )  ))    ; + op
-      (  (eq? '% (car stmt)) (% (filter  ) ( )  ))    ; + op
-
     )
   )
 )

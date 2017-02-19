ca2fb8809d290c080daf0c5b097fdd41a936272b
cxt240 <cxt240@case.edu>
2017-02-19 17:04:18 -0500

    compound statements
    If and while statements finished and associated calls added to
    statement.

--- Interpreter.scm
+++ Interpreter.scm
@@ -36,60 +36,67 @@
       ((zero? x) (cons val (cdr l)))
       (else (cons (car l) (setValue (cdr l) (- x 1)))))))
 
-
+; basic filter for instructions, filters out while and if statements, otherwise fowarded to varFunction
 (define statement
   (lambda (stmt stack)
     (cond
-      ((eq? 'while (car stmt)) 'while)
-      ((eq? 'if (car stmt)) (ifStmt (cadr stmt) (cadr (cdr stmt)) (cadr (cdr (cdr stmt))) stack))
-      (else (varFunction stmt stack)))))
+      ((eq? 'while (car stmt)) (while (cadr stmt) (cadr (cdr stmt)) stack))                        ; while function call
+      ((eq? 'if (car stmt)) (ifStmt (cadr stmt) (cadr (cdr stmt)) (cadr (cdr (cdr stmt))) stack))  ; if function call
+      (else (varFunction stmt stack)))))                                                           ; send to varFunction
 
+; if function that takes the condition, the then and else statements and a stack
 (define ifStmt
   (lambda (tfStmt stmt1 stmt2 stack)
     (cond
-      ((compound tfStmt stack) (statement stmt1 stack))
-      (else (statement stmt2 stack)))))
+      ((compound tfStmt stack) (statement stmt1 stack)) ; condition true, execute stmt1
+      (else (statement stmt2 stack)))))                 ; condition false, execute stmt 2
 
+; while function that takes the condition, the body of the loop and the stack
 (define while
   (lambda (tfStmt body stack)
     (cond
-      ((compound tfStmt stack) (while tfStmt body (statement body stack))) ;if the loop condition is true, execute the body statement
-      (else stack))))   ; otherwise return the stack
+      ((compound tfStmt stack) (while tfStmt body (statement body stack))) ; if the loop condition is true, execute the body statement
+      (else stack))))                                                      ; otherwise return the stack
  
 
+; assign, declare and return filter, otherwise the statement is invalid
 (define varFunction
   (lambda (stmt stack)
     (cond
-      ((eq? 'var (car stmt)) (declare (cadr stmt) stack))
-      ((eq? '= (car stmt)) (assign (cadr stmt) (cadr (cdr stmt)) stack))
-      ((eq? 'return (car stmt)) 'return)
-      (else (error invalid statement)))))
- 
+      ((eq? 'var (car stmt)) (declare (cadr stmt) stack))                   ; variable declaration
+      ((eq? '= (car stmt)) (assign (cadr stmt) (cadr (cdr stmt)) stack))    ; assignment operation
+      ((eq? 'return (car stmt)) (return stmt stack))                        ; return statment (creates a return variable, which is filtered in hte interpreter
+      (else (error '(invalid statement))))))
+
+; Declaring variable (initializes variable to 0)
 (define declare
   (lambda (var stack)
     (cond
-      ((null? var) (error invalid variable))
-      (else (list (cons var (car stack)) (cons '0 (cadr stack)))))))
+      ((null? var) (error '(invalid variable)))                           ; no variable declared, throw error
+      (else (list (cons var (car stack)) (cons '0 (cadr stack)))))))      ; add var and init to stack in respective places
 
+; Assigning a value to a variable given the variable name, the value to be assigned (can be a function), and the stack
 (define assign
   (lambda (var val stack)
     (cond
-      ((list? val) (list (car stack) (setValue (cadr stack) (identify val stack) (getIndex (car stack) var))))
-      ((exists? (car stack) var) (list (car stack) (setValue (cadr stack) val (getIndex (car stack) var))))
-      (else (error invalid variable)))))
+      ((list? val) (list (car stack) (setValue (cadr stack) (identify val stack) (getIndex (car stack) var))))    ; if the assignment is to be a function, find the value of the function before changing the value
+      ((exists? (car stack) var) (list (car stack) (setValue (cadr stack) val (getIndex (car stack) var))))       ; otherwise assign the value
+      (else (error '(invalid variable))))))                                                                       ; else throw an error     
 
+; return function, creates a variable called return
 (define return
   (lambda (stmt stack)
     (cond
-      ((null? stmt) (error bad statement))
-      ((assign 'return (identify (cadr stmt) stack) (declare 'return stack))))))
-                                              
+      ((exists? (car stack) 'return) stack)                                         ;if return already exists, do nothing
+      ((assign 'return (identify (cadr stmt) stack) (declare 'return stack))))))    ; initialize a return value and assign the return value
+
+; interpreter, takes a list of instructions and a blank stack ie '(() ())                                              
 (define instr
   (lambda (l stack)
     (cond
-      ((null? l) #f)
-      ((exists? (car stack) 'return) (getValue (cadr stack) (getIndex (car stack) 'return)))
-      (else (instr (cdr l) (statement (car l) stack))))))
+      ((null? l) '(no return))                                                                  ; no return in instruction
+      ((exists? (car stack) 'return) (getValue (cadr stack) (getIndex (car stack) 'return)))    ; if there's a return, just return the value, no more computation needed
+      (else (instr (cdr l) (statement (car l) stack))))))                                       ; else execute current instruction and do a recursive call for the next one
 
 ;-------------------------------------------------------------------------------------------------
 ;
